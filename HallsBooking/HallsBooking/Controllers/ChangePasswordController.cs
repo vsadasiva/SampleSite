7338486fbe31bc770cf3d3b937d979c8e6e80bd5
vsadasiva <sandhya.cheeti@raybiztech.com>
2016-06-23 17:48:21 +0530

    Committed changes to ChangePassword

--- HallsBooking/HallsBooking/Controllers/ChangePasswordController.cs
+++ HallsBooking/HallsBooking/Controllers/ChangePasswordController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HallsBooking;
 
 namespace HallsBooking.Controllers
 {
@@ -11,16 +10,28 @@ namespace HallsBooking.Controllers
     {
          //
         // GET: /ChangePassword/
-
+        SampleEntities2 db = new SampleEntities2();
         public ActionResult ChangePassword()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult ChangePassword()
+        public ActionResult ChangePassword(string OldPassword, string NewPassword, string ConfirmPassword)
         {
-
+            var pwd = db.Users.SingleOrDefault(x => x.RegId == 1);
+            if (pwd.Password == OldPassword)
+            {
+                pwd.Password = NewPassword;
+                pwd.ConfirmPassword = ConfirmPassword;
+                db.SaveChanges();
+            }
+            else
+            {
+                ViewBag.InValidOldPassword = "InValid Old Password";
+                return View("ChangePassword");
+            }
+            return View();
         }
 
     }
