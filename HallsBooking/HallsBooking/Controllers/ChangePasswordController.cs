e4cb15467408d42d0f9b8f4179a4a8665588f217
vsadasiva <sandhya.cheeti@raybiztech.com>
2016-06-23 10:50:28 +0530

    committed

---
+++ HallsBooking/HallsBooking/Controllers/ChangePasswordController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HallsBooking;
+
+namespace HallsBooking.Controllers
+{
+    public class ChangePasswordController : Controller
+    {
+         //
+        // GET: /ChangePassword/
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword()
+        {
+
+        }
+
+    }
+}
