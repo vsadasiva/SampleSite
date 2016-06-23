a2b8804658db31608ebc2d0d840ba02aa4ab5bc3
kalyani <kalyanijammula6@gmail.com>
2016-06-23 10:18:58 +0530

    Added HallsReg controller

---
+++ HallsBooking/HallsBooking/Areas/Agent/Controllers/HallsRegController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HallsBooking.Areas.Agent.Controllers
+{
+    public class HallsRegController : Controller
+    {
+        //
+        // GET: /Agent/HallsReg/
+
+        public ActionResult HallReg()
+        {
+            return View();
+        }
+
+    }
+}
