bf755008317dfd74c7267f268441ca17d615a932
kalyani <kalyanijammula6@gmail.com>
2016-06-24 09:59:30 +0530

    committed with halls reg

---
+++ HallsBooking/HallsBooking/Areas/Agent/Models/ImageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HallsBooking.Areas.Agent.Models
+{
+    public class ImageModel
+    {
+        [Required]
+        [DisplayName("Select File to Upload")]
+        public IEnumerable<HttpPostedFileBase> File { get; set; }  //2nd change
+    }
+}
