b10b92cff27213c45e43fc7eb12d0302cd6b5217
vsadasiva <sandhya.cheeti@raybiztech.com>
2016-06-29 18:02:32 +0530

    Committed BookingHall

---
+++ HallsBooking/HallsBooking/Areas/Agent/Controllers/BookHallController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HallsBooking.Areas.Agent.Models;
+
+namespace HallsBooking.Areas.Agent.Controllers
+{
+    public class BookHallController : Controller
+    {
+        //
+        // GET: /Agent/BookHall/
+
+        public ActionResult BookHall()
+        {
+            List<FunctionType> EnumValues = new List<FunctionType>();
+            EnumValues.Add(FunctionType.Birthday);
+            EnumValues.Add(FunctionType.GetToGather);
+            EnumValues.Add(FunctionType.Marriage);
+            EnumValues.Add(FunctionType.NamingCeremony);
+            EnumValues.Add(FunctionType.Reception);
+
+            ViewBag.FunctionType = EnumValues;
+            return View();
+        }
+    }
+}
---
+++ HallsBooking/HallsBooking/Areas/Agent/Models/BookHall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using HallsBooking;
+
+namespace HallsBooking.Areas.Agent.Models
+{
+    public enum FunctionType
+    {
+        Birthday,
+        Marriage,
+        Reception,
+        GetToGather,
+        NamingCeremony
+    }
+    public class BookHall
+    {
+        
+        public Hall Hall {get;set;}
+
+        public BookingDetail BookingDetail { get; set; }
+    }
+}
